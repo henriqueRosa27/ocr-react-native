856fda64930b11b44d3260c0dbc1c69b41b72076
Henrique <rosa.henrique@outlook.com.br>
2020-02-27 23:56:43 -0300

    Finalizado base para o app

--- App.js
+++ App.js
@@ -8,25 +8,103 @@ import {
   Alert,
   Text,
 } from 'react-native';
+import ActionSheet from 'react-native-actionsheet';
+import ImagePicker from 'react-native-image-picker';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.viewButton}>
-          <Button title="Carregar Foto" onPress={() => console.log(true)} />
-          <View style={{backgroundColor: 'white', flex: 0.02}} />
-          <Button
-            disabled={true}
-            title="Renderizar Imagem"
-            onPress={() => Alert.alert('Simple Button pressed')}
-          />
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
+class App extends React.Component {
+  showActionSheet = () => {
+    this.ActionSheet.show();
+  };
+
+  launchCamera = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+        });
+      }
+    });
+  };
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+        });
+      }
+    });
+  };
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.container}>
+          <View style={styles.viewButton}>
+            <Button title="Carregar Foto" onPress={this.showActionSheet} />
+            <View style={{backgroundColor: 'white', flex: 0.02}} />
+            <Button
+              disabled={true}
+              title="Renderizar Imagem"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View>
+            <ActionSheet
+              ref={o => (this.ActionSheet = o)}
+              title={'Escolha a fonte da foto'}
+              options={['Câmera', 'Biblioteca', 'Cancel']}
+              cancelButtonIndex={2}
+              destructiveButtonIndex={4}
+              onPress={index => {
+                if (index === 0) this.launchCamera();
+                else if (index === 1) this.launchImageLibrary();
+              }}
+            />
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
